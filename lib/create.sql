02fa30112c23951bc67f597da4ad018a09d3a928
Emmanuel Ngetich <engetich333@gmail.com>
2025-05-16 11:10:17 -0700

    sql

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+-- __define-ocg__ Table creation for bears
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  sex TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 8, 'M', 'black', 'aggressive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'gray', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 12, 'M', 'dark brown', 'strict', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'blonde', 'sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'M', 'greenish', 'grumpy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'F', 'white', 'playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7, 'F', 'black', 'mysterious', 1);
